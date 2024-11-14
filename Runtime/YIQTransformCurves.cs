9379551ce4201bcdd4e9a75b3d787caca468ccf9
nothke <nothke@gmail.com>
2024-11-14 13:17:43 +0100

    Initial

---
+++ Runtime/YIQTransformCurves.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nothke.CRT
+{
+    [CreateAssetMenu(fileName = "YIQPreset", menuName = "YIQ Transform Curves", order = 0)]
+    public class YIQTransformCurves : ScriptableObject
+    {
+        public AnimationCurve YCurve;
+        public AnimationCurve IQCurve;
+    }
+}
